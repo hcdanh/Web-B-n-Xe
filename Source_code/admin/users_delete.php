3d8db47f0b8b69cd46586ee135881896f0aaf190
duongnguyen11 <48851168+duongnguyen11@users.noreply.github.com>
2019-03-27 09:03:29 +0700

    chỉnh lại đúng form báo cáo

---
+++ Source_code/admin/users_delete.php
@@ -0,0 +1,27 @@
+<?php
+	include 'includes/session.php';
+
+	if(isset($_POST['delete'])){
+		$id = $_POST['id'];
+		
+		$conn = $pdo->open();
+
+		try{
+			$stmt = $conn->prepare("DELETE FROM users WHERE id=:id");
+			$stmt->execute(['id'=>$id]);
+
+			$_SESSION['success'] = 'Xóa tài khoản thành công';
+		}
+		catch(PDOException $e){
+			$_SESSION['error'] = $e->getMessage();
+		}
+
+		$pdo->close();
+	}
+	else{
+		$_SESSION['error'] = 'Chọn tài khoản cần xóa trước';
+	}
+
+	header('location: users.php');
+	
+?>
